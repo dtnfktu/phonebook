fa4a473464f7f15bb7dbf46e8d150432e08e16ef
Jeiojey <signofaries@gmail.com>
2022-10-02 12:38:54 +0300

    refactor: all ready

--- view.py
+++
@@ -1,8 +0,0 @@
-def get_name():
-    return input('Введите имя: ')
-
-def get_number():
-    return input('Введите номер телефона: ')
-
-def search_contact():
-    return input('Введите имя необходимого контакта: ')
